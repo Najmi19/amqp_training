fbdc009f1a516a4c61e0a5a8973f6df98b5e6ed4
Najmi Mehdi <najmimehdi70@gmail.com>
2021-12-08 15:08:40 +0100

    Vérifier l’asynchronisme

--- client/src/main/java/fr/lernejo/chat/Launcher.java
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -3,8 +3,11 @@ package fr.lernejo.chat;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 @SpringBootApplication
@@ -12,7 +15,9 @@ import java.util.Scanner;
 public class Launcher {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Launcher.class,args);
+        var ctx= new AnnotationConfigApplicationContext(Launcher.class);
+        RabbitTemplate template = ctx.getBean((RabbitTemplate.class));
         Scanner scanner = new Scanner(System.in);
         String input;
         boolean start = true;
@@ -21,8 +26,11 @@ public class Launcher {
 
         while(!(input.equals("q"))) {
             System.out.println("message envoyé, Saisissez un message, nous vous l'enverrons (q pour quitter)");
+            template.convertAndSend("","chat_messages",input);
             input = scanner.nextLine();
-
             }
+        System.out.println("ciao");
+        ctx.close();
         }
     }
+
